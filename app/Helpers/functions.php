97c847e89adc719999aee27ed4e269c509e11d65
isaac parker <isaacparker94@gmail.com>
2024-11-09 05:00:10 -0500

    csrf data

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,8 +1,40 @@
 <?php
-
+/**
+ * env call
+ * @param string $key
+ * @param string $default
+ * @return mixed
+ */
 function env(string $key,string $default = null)
 {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../..');
     $dotenv->load();
     return $_ENV[$key] ?? $default;
+}
+
+
+/**
+ * Create csrf Token
+ * @param string $token_name the form field name
+ * @return string
+ */
+function csrf(string $token_name): string {
+    $token  = new \App\Helpers\Classes\Token();
+    $token = $token->makeHash($token->generate(rand(4,16)));
+    \App\Helpers\Classes\Session::set($token_name,  $token[0], true);
+    return \App\Helpers\Classes\Session::get($token_name);
+}
+
+/**
+ * Check the csrf token
+ * @param string $token_name
+ * @return bool
+ */
+function check_csrf_token(string $token_name): bool {
+    if(\App\Helpers\Classes\Session::exists($token_name) &&   $_REQUEST[$token_name] === \App\Helpers\Classes\Session::get($token_name)){
+        \App\Helpers\Classes\Session::delete($token_name);
+        return true;
+    }
+    return false;
+
 }
